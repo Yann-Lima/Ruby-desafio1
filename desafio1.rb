741e1a530116dbaaf81430c894aa0c1e664be8ea
Yann Lima <96552505+Yann-Lima@users.noreply.github.com>
2023-10-14 22:05:19 -0300

    Desafio 1 Curso de Ruby

---
+++ desafio1.rb
@@ -0,0 +1,13 @@
+print "digite seu nome: "
+
+nome = gets.chomp
+
+print "digite seu sobrenome: "
+
+sobrenome = gets.chomp
+
+print "digite sua idade: "
+
+idade = gets.chomp
+
+puts "Seu nome é #{nome} #{sobrenome} e você tem #{idade}anos"
